00f8a58c9c72631302a53f596b62ec8bbd5b7f1a
lzh <luzehua@zhongbei.com>
2020-02-07 14:31:21 +0800

    Hello

---
+++ HelloWxmp/WXMPProject3/pages/data/newsData.js
@@ -0,0 +1,51 @@
+var initData = [
+  {
+    "newsid":0,
+    "authorIcon":"../images/head2.png",
+    "authorName" : "卢泽华",
+    "authorDate":"2019/12/07",
+    "title":"人生苦短,需要学习",
+    "articleImg":"../images/sub2.png",
+    "articleTitle": "人生苦短,需要学习人生苦短,需要学习人生苦短,需要学习人生苦短,需要学习人生苦短, 需                        要学习人生苦短, 需要学习人生苦短, 需要学习人生苦短,需要学习",
+    "articleLikeComment":"16",
+    "articleLikeSc": "38"
+  },
+  {
+    "newsid": 1,
+    "authorIcon": "../images/head1.png",
+    "authorName": "iwen",
+    "authorDate": "2019/12/06",
+    "title": "岁月不老,一世倾城",
+    "articleImg": "../images/sub1.png",
+    "articleTitle": "岁月不老,一世倾城岁月不老,一世倾城岁月不老,一世倾城岁月不老,一世倾城岁月不老,一世                      倾城岁月不老, 一世倾城岁月不老, 一世倾城岁月不老, 一世倾城",
+    "articleLikeComment": "12",
+    "articleLikeSc": "30"
+  },
+  {
+    "newsid": 2,
+    "authorIcon": "../images/head2.png",
+    "authorName": "卢泽华",
+    "authorDate": "2019/12/07",
+    "title": "人生苦短,需要学习",
+    "articleImg": "../images/sub2.png",
+    "articleTitle": "人生苦短,需要学习人生苦短,需要学习人生苦短,需要学习人生苦短,需要学习人生苦短, 需                        要学习人生苦短, 需要学习人生苦短, 需要学习人生苦短,需要学习",
+    "articleLikeComment": "16",
+    "articleLikeSc": "38"
+  },
+  {
+    "newsid": 3,
+    "authorIcon": "../images/head1.png",
+    "authorName": "iwen",
+    "authorDate": "2019/12/06",
+    "title": "岁月不老,一世倾城",
+    "articleImg": "../images/sub1.png",
+    "articleTitle": "岁月不老,一世倾城岁月不老,一世倾城岁月不老,一世倾城岁月不老,一世倾城岁月不老,一世                      倾城岁月不老, 一世倾城岁月不老, 一世倾城岁月不老, 一世倾城",
+    "articleLikeComment": "12",
+    "articleLikeSc": "30"
+  }
+]
+
+//导出可以被外部访问
+module.exports = {
+  initData: initData
+}
---
+++ HelloWxmp/WXMPProject3/pages/index/index.js
@@ -0,0 +1,11 @@
+Page({
+  goNewsTap: function (event) {
+    // console.log("跳转吧");
+    wx.navigateTo({
+      url: '../news/news',
+    })
+    // wx.switchTab({
+    //   url: '../news/news'
+    // })
+  }
+})
