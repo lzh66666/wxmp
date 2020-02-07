50d694135a30512b7a60db51fc16578cf004f890
lzh <luzehua@zhongbei.com>
2020-02-07 14:28:59 +0800

    Hello

---
+++ HelloWxmp/WXMPProject2/pages/news/news.js
@@ -0,0 +1,27 @@
+//导入数据
+var newsData = require("../data/newsData.js");
+
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    indicatorDots:true,
+    autoplay:true,
+    interval:2000,
+    circular:500,
+    indicatorColor:"rgba(0,255,0,1)",
+    indicatorActiveColor:"white",
+    useData:""
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    //this.setData可以让view重绘
+    this.setData({
+      useData:newsData.initData
+    })
+  }
+})
