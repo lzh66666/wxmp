4aab8d1c5615147dbc6391dd3619c5c9c538e05c
lzh <luzehua@zhongbei.com>
2020-02-07 14:57:03 +0800

    知了

---
+++ zhiliao/13douban/pages/index/index.js
@@ -0,0 +1,47 @@
+import {network} from "../../utils/network.js";
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+
+    // 电影
+    network.getMovieList({
+      success:function(movies){
+        that.setData({
+          movies:movies
+        })
+      }
+    });
+
+  // 电视剧
+    network.getTvList({
+      success:function(tvs){
+        that.setData({
+          tvs: tvs
+        })
+      }
+    });
+    
+  // 综艺
+    network.getShowList({
+      success:function(shows){
+        that.setData({
+          shows: shows,
+        })
+      }
+    })
+  },
+
+  
+})
